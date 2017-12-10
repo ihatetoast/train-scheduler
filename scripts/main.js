4a25c8c67e941a72457b387bdf1c0253361f1016
Katy <ihatetoast@gmail.com>
2017-12-10 11:29:48 -0600

    some changes to form and table.

--- scripts/main.js
+++ scripts/main.js
@@ -14,20 +14,29 @@ $(document).ready(function(){
 
     //database variables:
     const database = firebase.database();
-    
 
+    //app vars:
+    let frequency;
+
+
+    //moment variables:
+    const currentTime = moment();
+    
+    // var monthsWorked = moment(cs.val().start).diff(moment(), "months") * -1;
   // when the page loads, i want to pull data from firebase and populate my train schedule 
   //and when there's change
   //order by destination because that's what people would look up 
   //my story here is that this is seen in the main lobby of a train station
+
+  //the math happens when i bring things down from the farhbase
     database.ref().orderByChild("destination").on("child_added", function(childsnap){
       console.log(childsnap.val());
+      
       $("tbody").append(`
         <tr>
-          <td>${childsnap.val().train}</td>
-          <td>${childsnap.val().first}</td>
-          <td>${childsnap.val().destination}</td>
-          <td>${childsnap.val().frequency}</td>
+        <td>${childsnap.val().train}</td>
+        <td>${childsnap.val().destination}</td>
+        <td>${childsnap.val().frequency}</td>
           <td>fake time</td>
           <td>fake min left</td>
         </tr>
@@ -40,8 +49,7 @@ $(document).ready(function(){
   // i want to cancel if needed and clear the form
 
   //Code this app to calculate when the next train will arrive; this should be relative to the current time.
-  console.log(moment());
-  const currentTime = moment();
+  console.log(currentTime);
   console.log("CURRENT TIME: " + moment(currentTime).format("HH:mm"));
 
   // regex for 24h: 
@@ -51,12 +59,15 @@ $(document).ready(function(){
   //need to know when next train arrives from now. will need a first train time. 
 
   // on submit, i want to send the captured data to the databas and have that add to the schedule.$
-  let alertMessages = [];
+//TAs: 
+/*I don't really know how to do the form validation library, so I am making it up. I'd like to do something where they get the warning off focus, but it's a battle of time I have to get this done and time I have to break everything with the validator and then to fix it.*/
   $("#submit").click((e)=>{
     e.preventDefault();
+    //only 24hr time in HH:rr format
     if( !(/^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/.test($("#first").val().trim())) ){
       alert('Please use the 24-hour format.');
-    } else if ( ($("#train").val().trim()== '') || ($("#train").val().trim()== null) || ($("#destination").val().trim()== '') || ($("#destination").val().trim()== null) || ($("#frequency").val().trim()== '') || ($("#frequency").val().trim()== null) ){
+    } //no empty fields 
+    else if ( ($("#train").val().trim()== '') || ($("#train").val().trim()== null) || ($("#destination").val().trim()== '') || ($("#destination").val().trim()== null) || ($("#frequency").val().trim()== '') || ($("#frequency").val().trim()== null) ){
       alert('No empty values, please.');
     }    
     else{
@@ -67,16 +78,17 @@ $(document).ready(function(){
         destination: $("#destination").val().trim(),
         frequency: $("#frequency").val().trim(),
       }
+      console.log(`train line: ${formData.train}`);
+      console.log(`first train departs at: ${formData.first}`);
+      console.log(`frequency of train: ${formData.frequency}`);
       database.ref().push(formData);
-      $("#train").val('');
-      $("#first").val('');
-      $("#destination").val('');
-      $("#frequency").val('');
+      //clear after submit
+      $('input').val('');
       console.log(formData);
-  }
+    }
   });
 
-  $("#cancel").click(()=>{
+    $("#cancel").click(()=>{
     $("#train").val('');
     $("#first").val('');
     $("#destination").val('');
